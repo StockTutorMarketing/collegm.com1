073211d3c698a783deefbd3f06376d5ea98246a0
Kaustubhx <prime.ganekar@gmail.com>
2024-03-05 15:47:07 +0530

    Update: Refund Policy

--- app/(root)/refund/page.tsx
+++ app/(root)/refund/page.tsx
@@ -6,15 +6,56 @@ function Refund({ }: Props) {
         <div className='bg-gray-200 dark:bg-zinc-900 h-screen'>
             <div className='p-5 max-w-7xl mx-auto bg-white dark:bg-zinc-900 rounded-lg'>
                 <h1 className='uppercase text-3xl lg:text-5xl font-bold text-center'>REFUND</h1>
-                <div>
-                    <h3>Refund & Cancellation Policy</h3>
+                <div className='space-y-2'>
+                    <h3 className='text-3xl'>
+                        Cancellation
+                    </h3>
                     <p className='border-l-2 border-zinc-900 dark:border-zinc-100 p-2'>
-                        We don&apos;t provide refunds in any case. If you are not sure about the bootcamp, checkout the testimonials, read more about the trainer and take an informed decision. In case you miss out on attending the live bootcamp, we can move you to the next batch.
+                        Cancellation of any subscription is valid if requested before 14 days from the start of the course or program. After this period, no refunds will be applicable.
+                    </p>
+                </div>
+                <div className='space-y-2'>
+                    <h3 className='text-3xl'>
+                        Refunds for Partial Acces
+                    </h3>
+                    <p className='border-l-2 border-zinc-900 dark:border-zinc-100 p-2'>
+                        If any access is given to the user (e.g., videos, PDFs, codes, community, or any textual study material), a deduction of 30% will be applicable on the refund amount.
+                    </p>
+                </div>
+                <div className='space-y-2'>
+                    <h3 className='text-3xl'>
+                        Cancellation Process
+                    </h3>
+                    <p className='border-l-2 border-zinc-900 dark:border-zinc-100 p-2'>
+                        To cancel your subscription, please contact our support team at help@collegm.com with your request.
+                    </p>
+                    <p className='border-l-2 border-zinc-900 dark:border-zinc-100 p-2'>
+                        Please provide your payment details and the reason for cancellation.
+                    </p>
+                </div>
+                <div className='space-y-2'>
+                    <h3 className='text-3xl'>
+                        Refund Process
+                    </h3>
+                    <p className='border-l-2 border-zinc-900 dark:border-zinc-100 p-2'>
+                        Refunds will be processed within 7-10 business days after the cancellation request is approved.
+                    </p>
+                    <p className='border-l-2 border-zinc-900 dark:border-zinc-100 p-2'>
+                        Refunds will be issued to the original payment method used during the purchase.
+                    </p>
+                </div>
+                <div className='space-y-2 mt-2'>
+                    <p className='text-lg   border-l-2 border-zinc-900 dark:border-zinc-100 p-2'>
+                        <span className="font-semibold">
+                            Note: {" "}
+                        </span>
+                        This refund and cancellation policy is subject to change without notice. Please refer to our website for the most up-to-date policy.
                     </p>
                 </div>
             </div>
         </div>
     )
 }
+
 
 export default Refund
